15a6c44f82a0783a32e4b59162881abe511c7583
lbenav8095 <lubensaico@gmail.com>
2023-08-16 13:09:21 -0600

    Adds updater

--- src/main/java/us/mytheria/blobenderchest/BlobEnderchest.java
+++ src/main/java/us/mytheria/blobenderchest/BlobEnderchest.java
@@ -1,17 +1,21 @@
 package us.mytheria.blobenderchest;
 
+import org.jetbrains.annotations.NotNull;
 import us.mytheria.blobenderchest.director.ECManagerDirector;
+import us.mytheria.bloblib.entities.PluginUpdater;
 import us.mytheria.bloblib.managers.BlobPlugin;
 import us.mytheria.bloblib.managers.IManagerDirector;
 
 public final class BlobEnderchest extends BlobPlugin {
     private ECManagerDirector director;
     private IManagerDirector proxy;
+    private PluginUpdater updater;
 
     @Override
     public void onEnable() {
         director = new ECManagerDirector(this);
         proxy = director.proxy();
+        updater = generateGitHubUpdater("anjoismysign", "BlobEnderchest");
     }
 
     @Override
@@ -23,5 +27,11 @@ public final class BlobEnderchest extends BlobPlugin {
     @Override
     public IManagerDirector getManagerDirector() {
         return proxy;
+    }
+
+    @Override
+    @NotNull
+    public PluginUpdater getPluginUpdater() {
+        return updater;
     }
 }
